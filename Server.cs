81694fe50bf3b302dc92903a05222ceea4bd4648
Jeremy Gagnier <jer.gagnier@gmail.com>
2016-06-19 22:19:58 -0400

    Updated gitignore and beginning of user handling

--- Server.cs
+++ Server.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Collections.Generic;
 
 public class Server
 {
@@ -6,14 +8,21 @@ public class Server
     const string HELP_MESSAGE = @"h/help - help
 q - quit (shutdown server)
 live - how many users are online right now
+
 ";
 
     static int usersOnline = 0;
+    static List<SocketHandler.Controller> users = new List<SocketHandler.Controller>();
+    static Dictionary<string, Action<SocketHandler.Controller, string[]>> routes = 
+        new Dictionary<string, Action<SocketHandler.Controller, string[]>>()
+    {
+        { "queue", QueueUser },
+    };
 
     static void Main(string[] args)
     {
         SocketHandler.Server servSocket = new SocketHandler.Server(5287);
-        //servSocket.onNewConnection += StartNewSession;
+        servSocket.onNewConnection += StartNewSession;
 
         bool running = true;
         servSocket.onCloseConnection += (e) =>
@@ -31,6 +40,7 @@ live - how many users are online right now
         string message;
         while (running)
         {
+            Console.Write(">>> ");
             message = Console.ReadLine();
             if (message == "q")
             {
@@ -46,5 +56,29 @@ live - how many users are online right now
                 Console.WriteLine(usersOnline);
             }
         }
+    }
+
+    private static void StartNewSession(Socket socket)
+    {
+        SocketHandler.Controller controller = new SocketHandler.Controller(socket);
+        controller.onCloseConnection += (e) => LogOut(e, controller);
+        controller.onReceiveData += (s) => ProcessMessage(s, controller);
+        users.Add(controller);
+    }
+
+    private static void LogOut(Exception e, SocketHandler.Controller controller)
+    {
+        users.Remove(controller);
+    }
+
+    private static void ProcessMessage(string message, SocketHandler.Controller controller)
+    {
+        string[] args = message.Split(' ');
+        routes[args[0]](controller, args);
+    }
+
+    static void QueueUser(SocketHandler.Controller controller, string[] args)
+    {
+
     }
 }
